8d81a56e9a3f208825b903f42fe3a96b89d07e02
ESPORTS-148\cre <t-shirai@oca.ac.jp>
2022-12-13 17:34:01 +0900

    バグ４

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -43,8 +43,9 @@ class LoginController extends Controller
     /**
      * ログアウト処理
      */
-    public function logout()
+    public function logout(Request $request)
     {
+        $request->session()->flush();
         return redirect('/login');
     }
 }
